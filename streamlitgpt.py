c577f512a9ae11434e7e16b76cd8b16e8a0a4b64
rajuptvs <raju.ptvs@gmail.com>
2022-12-11 13:47:52 -0800

    testing out audio recording

---
+++ streamlitgpt.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import os
+import streamlit.components.v1 as components
+
+
+def audio_test():
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
+    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
+    st.title('Audio Test')
+    st.write('\n \n')
+    
+    instance=st_audiorec()
